db0e1eea283c1521db83d15d38baf22ce5896571
Kenta Onimura <oniken@Cs-212-37.CS.UCLA.EDU>
2015-04-17 16:37:42 -0700

    Started new blank rails app

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-Atticus::Application.initialize!
+Rails.application.initialize!
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Atticus::Application.config.session_store :cookie_store, key: '_atticus_session'
+Rails.application.config.session_store :cookie_store, key: '_atticus_session'
