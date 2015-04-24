b10fde5013b6b8dadd614899fb15251ef781f94b
lawrencey99 <lawrencey99@gmail.com>
2015-04-24 01:26:15 -0700

    added login entries to database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+
+  devise_scope :user do
+    get "/login" => "devise/sessions#new"
+    delete "/logout" => "devise/sessions#destroy"
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
