a71a5cdb06e6e29ad01dd133eabc58fabc9be1ee
Kenta Onimura <oniken@Kentas-MacBook-Pro.local>
2015-04-24 17:01:26 -0700

    Commented out all tests

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,2 +1,9 @@
 class Contract < ActiveRecord::Base
+	validates :title, :description, :image_url, :company, presence: true
+	validates :pay, numericality: {greater_than_or_equal_to: 0.01}
+	validates :title, uniqueness: true
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)$}i,
+		message: 'must be a url for GIF, JPG, or PNG image.'
+	}
 end
