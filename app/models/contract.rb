2d2d5c8f800c2a55d3e1b40a25f8e7e0c1f971b7
Kenta Onimura <oniken@Kentas-MacBook-Pro.local>
2015-04-24 17:22:54 -0700

    Fixed validation security

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -3,7 +3,7 @@ class Contract < ActiveRecord::Base
 	validates :pay, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
 	validates :image_url, allow_blank: true, format: {
-		with: %r{\.(gif|jpg|png)$}i,
+		with: %r{\.(gif|jpg|png)\Z}i, 
 		message: 'must be a url for GIF, JPG, or PNG image.'
 	}
 end
