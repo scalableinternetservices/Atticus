d752bdc3253216c044554c1c16b76a40b533b878
lawrencey99 <lawrencey99@gmail.com>
2015-05-01 01:04:04 -0700

    adding students scaffolding and registration redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   
+  resources :students
   resources :contracts
 
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
 
   devise_scope :user do
     get "/login" => "devise/sessions#new"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150430183044) do
+ActiveRecord::Schema.define(version: 20150501071320) do
 
   create_table "contracts", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20150430183044) do
     t.decimal  "pay",                     precision: 10
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string   "email",       limit: 255
+    t.string   "school",      limit: 255
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   create_table "users", force: :cascade do |t|
