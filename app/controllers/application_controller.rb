cc275977d2e2b33aad083e62aaa17384c11aa673
Kenta Onimura <oniken@Cs-212-30.CS.UCLA.EDU>
2015-05-11 09:24:07 -0700

    fixed home icon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  # TODO: Turn off for Tsung testing
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
