f6f2d1252873d376a3503ba60b64d2229b30afa4
Kenta Onimura <oniken@Kentas-MacBook-Pro.local>
2015-05-14 11:44:40 -0700

    Updated links for adding contracts and different sidebar for companies

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -24,15 +24,24 @@ class ContractsController < ApplicationController
   # POST /contracts
   # POST /contracts.json
   def create
-    @contract = Contract.new(contract_params)
-
-    respond_to do |format|
-      if @contract.save
-        format.html { redirect_to @contract, notice: 'Contract was successfully created.' }
-        format.json { render :show, status: :created, location: @contract }
-      else
-        format.html { render :new }
-        format.json { render json: @contract.errors, status: :unprocessable_entity }
+    # Only companies can create contracts
+    if !current_user.is_student
+      @contract = Contract.new(contract_params)
+      @contract.owner = current_user.id
+      current_user.contracts << @contract
+      respond_to do |format|
+        if @contract.save
+          format.html { redirect_to @contract, notice: 'Contract was successfully created.' }
+          format.json { render :show, status: :created, location: @contract }
+        else
+          format.html { render :new }
+          format.json { render json: @contract.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+      respond_to do |format|
+          format.html { render :index }
+          format.json { render json: @contract.errors, status: :unprocessable_entity }
       end
     end
   end
