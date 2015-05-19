d3c660cb6ae32bbca4c45b850f95c4cc253533f2
Kenta Onimura <kentaonex@gmail.com>
2015-05-19 14:40:45 -0700

    Styled comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150519204904) do
+ActiveRecord::Schema.define(version: 20150519204844) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "message",     limit: 65535
@@ -59,11 +58,6 @@ ActiveRecord::Schema.define(version: 20150519204904) do
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
-  end
-
-  create_table "profiles", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "industry_tags_users", id: false, force: :cascade do |t|
