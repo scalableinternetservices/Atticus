1c3948de60b3388f9b5ccf8dadd58a3f51a48ff8
Kenta Onimura <oniken@Kentas-MacBook-Pro.local>
2015-05-19 11:52:32 -0700

    ran migration again with schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519172147) do
+ActiveRecord::Schema.define(version: 20150519010539) do
 
   create_table "contracts", force: :cascade do |t|
     t.string   "title",       limit: 255
