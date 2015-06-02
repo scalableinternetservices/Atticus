13d669c4a6229cb3158ab9d5376cc0a642be7a0b
Kenta Onimura <oniken@Kentas-MacBook-Pro.local>
2015-06-02 11:00:26 -0700

    Improved user search views

--- app/helpers/contracts_helper.rb
+++ app/helpers/contracts_helper.rb
@@ -1,2 +1,9 @@
 module ContractsHelper
+  def cache_key_for_contracts
+    "contracts-#{Contract.maximum(:updated_at)}-#{Comment.maximum(:updated_at)}"
+  end
+
+  def cache_key_for_contract_row(contract)
+    "contract-#{contract.id}-#{contract.updated_at}-#{contract.comments.count}"
+  end
 end
